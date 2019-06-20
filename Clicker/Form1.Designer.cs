2f7da4011613ab56fe73ab306f26300d7cc5090c
VerSS <verss560@gmail.com>
2019-06-20 22:31:17 +0300

    first commit

---
+++ Clicker/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Clicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(164, 247);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Тык";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Azure;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.ForeColor = System.Drawing.Color.Green;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(137, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "0$";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(246, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(62, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "x3(70$)";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Control;
+            this.button3.Location = new System.Drawing.Point(246, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(62, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "x2(50$)";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.Control;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.button4.Location = new System.Drawing.Point(246, 70);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(62, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "x10(300$)";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = global::Clicker.Properties.Resources.ClickerBackground;
+            this.ClientSize = new System.Drawing.Size(311, 299);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(327, 338);
+            this.MinimumSize = new System.Drawing.Size(327, 0);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Clicker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Clicker/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clicker
+{
+    public partial class Form1 : Form
+    {
+        public Random rnd = new Random();
+        public long money = 0; // игровая валюта
+        public long x2 = 0; // количество х2 бустеров
+        public long x3 = 0; // количество х3 бустеров
+        public long x10 = 0; // количество х10 бустеров
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) // добавление валюты
+        {
+            button1.BackColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+            if (x2 == 0 && x3 == 0 && x10 == 0)
+            {
+                money = money + 1;
+            }
+            if (x2 >= 1 && x3 == 0 && x10 == 0)
+            {
+                money = money + 1 * (2 * x2); 
+            }
+            if (x2 == 0 && x3 >= 1 && x10 == 0)
+            {
+                money = money + 1 * (3 * x3);
+            }
+            if (x2 == 0 && x3 == 0 && x10 >= 1)
+            {
+                money = money + 1 * (10 * x10);
+            }
+            if(x2 >= 1 && x3 >=1 && x10 == 0)
+            {
+                money = money + 1 * (2 * x2) * (3 * x3);
+            }
+            if (x2 == 0 && x3 >= 1 && x10 >= 1)
+            {
+                money = money + 1 * (3 * x3) * (10 * x10);
+            }
+            if (x2 >= 1 && x3 == 0 && x10 >= 1)
+            {
+                money = money + 1 * (2 * x2) * (10 * x10);
+            }
+            if (x2 > 0 && x3 > 0 && x10 > 0)
+            {
+                money = money + 1 * (2 * x2) * (3 * x3) * (10 * x10);
+            }
+            textBox1.Text = money + "$";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) //x3
+        {
+            if(money >= 70)
+            {
+                money = money - 70;
+                textBox1.Text = money + "$";
+                x3++;
+                MessageBox.Show($"x3 упешно куплен, всего x3 - {x3}");
+            }
+            else
+            {
+                MessageBox.Show("Недостаточно денег для покупки.");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) //x2
+        {
+            if(money >= 50)
+            {
+                money = money - 50;
+                textBox1.Text = money + "$";
+                x2++;
+                MessageBox.Show($"x2 упешно куплен, всего х2 - {x2}");
+            }
+            else
+            {
+                MessageBox.Show("Недостаточно денег для покупки.");
+            }
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e) // x10
+        {
+            if (money >= 300)
+            {
+                money = money - 300;
+                textBox1.Text = money + "$";
+                x10++;
+                MessageBox.Show($"x10 упешно куплен, всего x10 - {x10}");
+            }
+            else
+            {
+                MessageBox.Show("Недостаточно денег для покупки.");
+            }
+        }
+    }
+}
